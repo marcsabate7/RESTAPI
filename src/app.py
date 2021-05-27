2b8e4d7366b86c86de7909f667e47786d14eb474
Marc <marc-saba@hotmail.com>
2021-05-27 17:57:31 +0200

    modified methods to have the id's more importances at the moment to make wuerys

--- src/app.py
+++ src/app.py
@@ -24,14 +24,14 @@ def list_exams():
 @app.route('/exam', methods=['POST'])
 def createExam():
     count = 0
-    # The unique key generated for the new uploaded exam is the ID
+    # The unique key generated for the new uploaded exam is the id_exam referenced in all code
     try:
         name_exam = request.json['name_exam']
         query = mongo.db.exams.find({"name_exam":name_exam})
         for item in query:
             count+=1
         if count !=0:
-            return {"message":"Exam is added to the database so you CAN'T add this exam with the same name"}
+            return {"message":"This exam is already added to the database, try another name"}
         else:
             description = request.json['description']
             date = request.json['date']
@@ -54,42 +54,55 @@ def createExam():
             })
             return response
     except:   
-        return {"message":"You miss some fields to add an exam or some one are incorrect!"}
+        return notFound()
 
 @app.route('/modifydescription', methods=['POST'])
 def modifyDescriptionExam():
-    name_exam = request.json['name_exam']
-    description = request.json['description']
-    if name_exam!= "" and description != "":
-        mongo.db.exams.update_one({
-            "name":name_exam
-        },
-        {"$set": {'description':description}})
+    try:
+        exam_exists = False
+        id_exam = request.json['id_exam']
+        description = request.json['description']
+        query = mongo.db.exams.find({"key_generated":id_exam})
+        for item in query:
+            exam_exists = True
+        if exam_exists and id_exam!= "" and description != "":
+            mongo.db.exams.update_one({
+                "key_generated":id_exam
+            },
+            {"$set": {'description':description}})
 
-        #mongo.db.exams.find({"name":name})
+            return {"message":"Description modifyed correctly for exam with ID: "+str(id_exam)}
 
-        return {"message":"Description modifyed correctly"}
-
-    else:
-        return {"message":"YOU MISS SOME FIELDS TO ADD AN EXAM!","parameters":"name_exam and description"}
+        else:
+            response = jsonify({
+                "message":"Exam doesn't exist so we CAN'T modify the description",
+                "parameters":"id_exam, description"
+            })
+            response.status_code = 404
+            return response
+    except:
+        return notFound()
 
 @app.route('/deletexam', methods=['POST'])
 def deleteExam():
-    exam_exists = False
-    name_exam = request.json['name_exam']
-    query = mongo.db.students.find({"grades":{"$elemMatch":{"name_exam":name_exam}}})
-    for item in query:
-        print(item)
-        return {"message":"Can NOT delete this exam because some students have grades in it"}
-    
-    query2 = mongo.db.exams.find({"name_exam":name_exam})
-    for item in query2:
-        exam_exists = True
-    if exam_exists:
-        mongo.db.exams.remove({"name_exam":name_exam})
-        return {"message":"Exam has been deleted"}
-    else:
-        return {"message":"This exam dosn't exist so can't be deleted"}
+    try:
+        exam_exists = False
+        id_exam = request.json['id_exam']
+        query = mongo.db.students.find({"grades":{"$elemMatch":{"id_exam":id_exam}}})
+        for item in query:
+            print(item)
+            return {"message":"CAN'T delete this exam because some students have grades in it"}
+        
+        query2 = mongo.db.exams.find({"key_generated":id_exam})
+        for item in query2:
+            exam_exists = True
+        if exam_exists:
+            mongo.db.exams.remove({"key_generated":id_exam})
+            return {"message":"Exam has been deleted"}
+        else:
+            return {"message":"This exam dosn't exist so can't be deleted"}
+    except:
+        return notFound()
 
 ######################################
 #            STUDENTS
@@ -100,11 +113,8 @@ def createStudent():
     count = 0
     try:
         name_student = request.json['name_student']
-        query = mongo.db.students.find({"name_student":name_student})
-        for item in query:
-            count+=1
-        if count !=0 or name_student== "":
-            return {"message":"This student is added to the database / or the name is empty, try another one"}
+        if name_student== "":
+            return {"message":"The name of the student is empty, add name"}
         else:
             total_students = mongo.db.students.count()+1
             id = mongo.db.students.insert(
@@ -113,40 +123,48 @@ def createStudent():
         
             response = jsonify({
                 'name_student':name_student,
+                'key_generated': total_students,
                 'status_query': 'OK DONE'
             })
+            response.status_code = 200
             return response
     except:   
-        return {"message":"YOU MISS SOME FIELDS TO ADD AN USER!"}
+        return notFound()
 
 
 @app.route('/addgrade', methods=['POST'])
 def addGradeStudent():
-    exam_exists = False
-    name_student = request.json['name_student']
-    name_exam = request.json['name_exam']
-    grade = request.json['grade']
-    query = mongo.db.exams.find({"name_exam":name_exam})
-    for item in query:
-        exam_exists = True
-    
-    if exam_exists and name_student!= "" and grade!= "":
-        toinsert = {'name_exam':name_exam,'grade':grade}
-        mongo.db.students.update(
-            {"name_student":name_student}, {'$push': {'grades': toinsert}})
+    try:
+        exam_exists = False
+        id_student = request.json['id_student']
+        id_exam = request.json['id_exam']
+        grade = request.json['grade']
+        query = mongo.db.exams.find({"key_generated":id_exam})
+        for item in query:
+            exam_exists = True
+        
+        if exam_exists and id_student!= "" and grade!= "":
+            toinsert = {'id_exam':id_exam,'grade':grade}
+            mongo.db.students.update(
+                {"key_generated":id_student}, {'$push': {'grades': toinsert}})
 
-        #mongo.db.exams.find({"name":name})
+            return {"message":"Grade: " + str(grade) + " added correctly to student ID: "+ str(id_student) +", for exam ID: " +str(id_exam)}
 
-        return {"message":"Grade added correctly to student "+name_student}
-
-    else:
-        return {"message":"Exam doesn't exist or name_student or grade are empty","parameters":"name_student, name_exam, grade"}
+        else:
+            response = jsonify({
+                "message":"Exam doesn't exist or id_student or grade are empty",
+                "parameters":"id_student, id_exam, grade"
+            })
+            response.status_code = 404
+            return response
+    except:
+        return notFound()
 
 
 @app.errorhandler(404)
-def not_found(error=None):
+def notFound(error=None):
     response = jsonify({
-        'message': 'Resource Not Found: ' + request.url,
+        'message': 'Resource Not Found, you miss some data to the JSON, requested url: ' + request.url,
         'status': 404,
     })
     response.status_code = 404
