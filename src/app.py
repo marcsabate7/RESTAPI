172f721afbddf60faafcb48c605c5f00cd32baed
Marc <marc-saba@hotmail.com>
2021-05-28 10:18:25 +0200

    modyfied some parts in order to perform methods

--- src/app.py
+++ src/app.py
@@ -102,7 +102,7 @@ def modifyDescriptionExam():
     except:
         return notFound()
 
-@app.route('/deletexam', methods=['POST'])
+@app.route('/deletexam', methods=['DELETE'])
 def deleteExam():
     try:
         exam_exists = False
@@ -151,10 +151,11 @@ def createStudent():
         return notFound()
 
 
-@app.route('/addgrade', methods=['POST'])
+@app.route('/addgrade', methods=['PUT'])
 def addGradeStudent():
     try:
         exam_exists = False
+        grade_iscreated = False
         id_student = request.json['id_student']
         id_exam = request.json['id_exam']
         grade = request.json['grade']
@@ -163,11 +164,24 @@ def addGradeStudent():
             exam_exists = True
         
         if exam_exists and id_student!= "" and grade!= "":
-            toinsert = {'id_exam':id_exam,'grade':grade}
-            mongo.db.students.update(
-                {"key_generated":id_student}, {'$push': {'grades': toinsert}})
+            query2 = mongo.db.students.find({"key_generated":id_student})
+            for item in query2:
+                response = json_util.dumps(item) 
+                grades = json.loads(response)
+                grades = grades['grades']
+                for grade in grades:
+                    id_exam2 = grade['id_exam']
+                    if id_exam == id_exam2:
+                        grade_iscreated = True
+                        break
+            if grade_iscreated:
+                return {"message":"This grade is created so you CAN'T add another grade from the same exam, please update it if you want"}
+            else:
+                toinsert = {'id_exam':id_exam,'grade':grade}
+                mongo.db.students.update(
+                    {"key_generated":id_student}, {'$push': {'grades': toinsert}})
 
-            return {"message":"Grade: " + str(grade) + " added correctly to student ID: "+ str(id_student) +", for exam ID: " +str(id_exam)}
+                return {"message":"Grade: " + str(grade) + " added correctly to student ID: "+ str(id_student) +", for exam ID: " +str(id_exam)}
 
         else:
             response = jsonify({
@@ -176,6 +190,45 @@ def addGradeStudent():
             })
             response.status_code = 404
             return response
+    except:
+        return notFound()
+
+
+@app.route('/modifygrade', methods=['PUT'])
+def modifyGrade():
+    try:
+        exam_exists = False
+        comprovation_exam = False
+        all_grades = []
+        id_student = request.json['id_student']
+        id_exam = request.json['id_exam']
+        grade_st = request.json['grade']
+        query = mongo.db.exams.find({"key_generated":id_exam})
+        for item in query:
+            comprovation_exam = True
+        if comprovation_exam == True and id_student != "" and id_exam != "" and grade_st != "":
+            query2 = mongo.db.students.find({"key_generated":id_student})
+            for item in query2:
+                response = json_util.dumps(item) 
+                grades = json.loads(response)
+                grades = grades['grades']
+                for grade in grades:
+                    id_exam2 = grade['id_exam']
+                    if id_exam == id_exam2 and exam_exists == False:
+                        grade['grade'] = grade_st
+                        exam_exists = True
+                    all_grades.append(grade)
+                
+            if exam_exists:
+                mongo.db.students.update_one(
+                    {"key_generated":id_student},
+                    {"$set": {'grades':all_grades}})
+                return {"message":"Grade: " + str(grade_st) + " for ID student: "+ str(id_student) + " and ID exam: " + str(id_exam) + " was updated succesfully!!"}
+            else:    
+                return {"message":"The id_exam doesn't match with the id_student grades"}
+        else:
+            return {"message":"Doesn't exist an exam with this ID or some JSON fields are in blank"}
+    
     except:
         return notFound()
 
@@ -198,7 +251,7 @@ def downloadGrades():
             grades = json_util.dumps(grades) 
             return grades
         else:
-            return {"message":"This ID don't EXIST put a good one!"}
+            return {"message":"This id_student don't EXIST put a good one!"}
     except:
         return notFound()
 
